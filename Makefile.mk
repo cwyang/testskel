01efe8bb47b212fc7f6d7d9a1d60821b81c7d18c
Chul-Woong Yang <cwyang@gmail.com>
2023-07-11 00:02:00 +0900

    Makefile

--- Makefile.mk
+++ Makefile.mk
@@ -2,8 +2,11 @@
 # 2 July 2023
 # Chul-Woong Yang
 
-export PROGNAME = dummy.sh
-export PERL5LIB = /home/cwyang/perl5/lib/perl5
+VPP_PATH=/home/vagrant/vpp-dev/build-root/install-vpp_debug-native/vpp/bin
+export VPP = ${VPP_PATH}/vpp
+export VPPCTL = ${VPP_PATH}/vppctl
+export PROGS = ${VPP} ${VPPCTL}
+export PERL5LIB = $(shell realpath ~/perl5/lib/perl5)
 
 .NOTPARALLEL:
 .PHONY : check_precondition check
@@ -13,7 +16,7 @@ all: check_precondition
 check_precondition:
 	@perl -MNet::EmptyPort -MPath::Tiny -MScope::Guard -MStarlet -MNet::DNS::Nameserver /dev/null > /dev/null 2>&1 || \
 	(echo; \
-	 echo "Please install following Perl modules: Net::EmptyPort Net::DNS::Nameserver Path::Tiny Scope::Guard Starlet"; \
+	 echo "Please install following Perl modules: Net::EmptyPort Net::DNS::Nameserver Path::Tiny IO::Socket::SSL Scope::Guard Starlet"; \
 	 echo && exit 1)
 	@which plackup socat curl > /dev/null 2>&1 || \
 	(echo; \
@@ -21,9 +24,13 @@ check_precondition:
 	 echo && exit 1)
 
 install_pm:
-	cpanm install Net::EmptyPort Path::Tiny Scope::Guard Starlet Net::DNS::Nameserver
+	sudo apt-get install cpanminus
+	cpanm install Net::EmptyPort Path::Tiny Scope::Guard Starlet Net::DNS::Nameserver IO::Socket::SSL
 
 check:
 	sudo t/gen-environ.sh
-	sudo -E ip netns exec client bash -c "PERL5LIB=${PERL5LIB} time t/run-tests t/*.t"
-
+	echo ${VPP} ${VPPCTL}
+	echo ${ARGS}
+#	sudo -E ip netns exec client bash -c "PERL5LIB=${PERL5LIB} time t/run-tests t/*.t"
+#	sudo -E ip netns exec client bash -c "PERL5LIB=${PERL5LIB} time t/run-tests t/10env.t"	
+	sudo -E ip netns exec client bash -c "PERL5LIB=${PERL5LIB} time t/run-tests t/50_1_base.t"	
