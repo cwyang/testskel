5ef79d064e4432699d3015b316d748a382a44d25
Chul-Woong Yang <cwyang@gmail.com>
2023-07-27 06:40:21 +0000

    timeout

--- t/udp_client.pl
+++ t/udp_client.pl
@@ -10,9 +10,17 @@ my $socket = IO::Socket::INET->new(
 
 print STDERR "UDP echo client is connecting on $ip $port\n";
 
+local $SIG{ALRM} = sub {
+    $gotsig = 1;
+    die "gotsig";
+};
+
+alarm(5);
+
 $socket->send($msg);
 my $data;
 my $client_address = $socket->recv($data, 1024);
 print $data;
 
 $socket->close();
+alarm(0);
